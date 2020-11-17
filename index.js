6210e3148d9ec4500429fe57a133e06660f594e6
kimchaeru <kmc280709@gmail.com>
2020-11-17 13:02:01 +0900

    boy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const client = new Discord.Client({disableEveryone: true});
 client.commands = new Discord.Collection();
 const info = require('./Discord.js/Packing.js')
-const { token, prefix } = process.env.token;
+const token = process.env.token;
 
 client.on('ready', () => {
 	fs.readFile('./Discord.js/LICENSE', function(err, data) {
